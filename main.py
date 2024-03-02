8eed973575c1b4c42669ebcebaf8530c1661c6af
Songhan ZHANG <shz@dlut.edu.cn>
2024-03-02 23:41:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ Materials = np.array([ [ 1, (2e11, 7850, 0.3) ] ] , dtype = object)
 Reals = np.array([ [ 1, (1) ] ], dtype = object)
 
 # plt.figure(figsize = (6.4,6.4))
+
 """
 fig, ax = plt.subplots(figsize = (6.4,6.4))
 ax.tick_params(direction = 'in')
